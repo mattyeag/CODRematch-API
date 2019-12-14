bd1f3012621779c510f60396e24e4c6a7c026342
Matthew <matthew@Johns-MacBook-Pro.local>
2019-12-14 16:35:52 -0500

    added call to get friends stats from COD api

--- src/routes/public.ts
+++ src/routes/public.ts
@@ -19,5 +19,19 @@ publicRoutes.post('/cod/stats', async (req, res) =>{
     }
 })
 
-publicRoutes.get('/friends', (req, res) =>{console.log("get player friends"); res.send("DONE")})
+publicRoutes.post('/cod/friends/stats', async (req, res) =>{
+    var params : PlayerParams = {userName: req.body.username, gameTitle: req.body.game, platform: req.body.platform  }
+     try{
+ 
+         let response = await player.getPlayersFriendsStats(params)
+         if(response.status == 'success'){
+             res.send(response);
+         }else{
+             res.send({status:'error', message: `error calling player stats api. Response: ${JSON.stringify(response)}`}); 
+         }
+     }catch(error){
+         console.error("error calling to get player stats", error); 
+     }
+ })
+ 
 
