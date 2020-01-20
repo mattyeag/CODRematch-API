de089042c220e69b0edacc2fc60b049ad34ff2cf
Matthew <matthew@Johns-MacBook-Pro.local>
2020-01-19 20:03:18 -0500

    added /health endpoint and cron job for keep alive

--- ExpressApp.ts
+++ ExpressApp.ts
@@ -3,6 +3,14 @@ import {publicRoutes} from './src/routes/public'
 import {privateRoutes} from './src/routes/private'
 import * as bodyParser from 'body-parser'; 
 import * as dotenv from 'dotenv'; 
+const cron = require('node-cron');
+
+
+console.log("starting cron job..."); 
+    cron.schedule("*/2 * * * *", ()=>{
+        console.log("app is alive at time: " + new Date())
+})
+
 
 dotenv.config(); 
 export const app = express()
@@ -10,3 +18,4 @@ app.use(bodyParser.json());
 
 app.use('/public', publicRoutes); 
 app.use('/private', privateRoutes);  
+app.use('/health', (err,res)=>{console.log("app is healthy"); res.sendStatus(200)})
